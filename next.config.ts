130c6442a24b0bfc7cfdd65401bcd7a3e7fd0978
shraxmatov <sh.raxmatov@proximaops.io>
2026-03-10 17:10:29 +0500

    Adding ci/cd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   sassOptions: {
     additionalData: `@use "${path.resolve(process.cwd(), 'styles/_variables.scss').replace(/\\/g, '/')}" as *;`,
   },
